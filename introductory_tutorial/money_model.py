dde75206d03e4957d44b7fe81c2af2f6ba44faba
Sangkon Han <sigmadream@gmail.com>
2023-06-19 15:42:18 +0900

    Adding the scheduler

--- introductory_tutorial/money_model.py
+++ introductory_tutorial/money_model.py
@@ -6,9 +6,17 @@ class MoneyAgent(mesa.Agent):
         super().__init__(unique_id, model)
         self.wealth = 1
 
+    def step(self):
+        print(f"Hi, I am agent {self.unique_id}.")
+
 
 class MoneyModel(mesa.Model):
-    def __int__(self, N):
+    def __init__(self, N):
         self.num_agents = N
+        self.schedule = mesa.time.RandomActivation(self)
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
+            self.schedule.add(a)
+
+    def step(self):
+        self.schedule.step()
